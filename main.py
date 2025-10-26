405d2a9335e7a24f85bd84c1d74128e818d7aeae
sindicate07 <r2ghpzkwkf@privaterelay.appleid.com>
2025-10-26 03:07:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -32,15 +32,13 @@ async def on_ready():
 
 GUILD_ID = discord.Object(id=1315069563280556072)
 
-# colors: lulu 0x8B463C, liz 0x7084B1,
-
 # embed function
 
 
-def embed_func(char, msg, color, char_img, leng):
+def embed_func(char, msg, color, char_img, img_leng):
     embed = discord.Embed(title=char, description='\"*' +
                           msg+'*\"', colour=color)
-    embed.set_thumbnail(url=char_img[random.randint(0, leng)])
+    embed.set_thumbnail(url=char_img[random.randint(0, img_leng)])
     return embed
 
 
@@ -52,7 +50,6 @@ lulu_pics = [
     "https://images-ext-1.discordapp.net/external/8AzckEjq3FnJr7eKoIC_CIpqEmJCYQ412reg_aH5XwU/https/i.imgur.com/vFqiPJS.png;https",
     "https://images-ext-1.discordapp.net/external/GjWBO_01QnZ8PckogcVoozeHYzh1c8MluEWEqadxAcU/https/i.imgur.com/HGgZvHL.png"
 ]
-
 liz_pics = [
     "https://images-ext-1.discordapp.net/external/4FOCdPuFHkGztRiBdydPQO6K2JPaYIlJ__lr1qnrLmg/https/i.imgur.com/jZmYP9u.png",
     "https://images-ext-1.discordapp.net/external/m5q1ER9-_7JFO-dWsD9MbvuKclRTeg_3PF3xnO98Qfg/https/i.imgur.com/Cl6NB2M.png",
@@ -61,63 +58,180 @@ liz_pics = [
     "https://images-ext-1.discordapp.net/external/W-tWhKfS9kqQktq-FCdA9LZ_7V3PlCVjGIlxcnTTNrA/https/i.imgur.com/5B4bTX9.png",
     "https://images-ext-1.discordapp.net/external/MyBG3gSWU1Q-c7FrbxgW1M1icd-ksYVNUPi4lHaZ4U4/https/i.imgur.com/4kKRxLz.png"
 ]
+agnes_pics = [
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431938931108216894/agnes_bottom_og1.png?ex=68ff3c8d&is=68fdeb0d&hm=732418916587fb1ec34b2b39c96384cb6f6712e93581d2e8b54d9c4571052a3b&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431938954025893938/References_202312032037501.png?ex=68ff3c92&is=68fdeb12&hm=0d6ce831553a16896b0d6e66cbb34c5f3fa1bacce0b0fdb7538de06d58b3d4cf&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431938985457877065/References_202312032037502.png?ex=68ff3c9a&is=68fdeb1a&hm=5860b6bc2ebf6cbeb39d70d04f02baeaa789077ae5a8eb86c1d27cfba1fda4a3&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431939006676996096/References_202312032037503.png?ex=68ff3c9f&is=68fdeb1f&hm=ce750f7f30315aa312cb9137143fdd99f9ee780bbeeb8c0b9c0775c8bea6da1d&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431939030727266396/References_202312032037504.png?ex=68ff3ca5&is=68fdeb25&hm=86a6c29b84b2c75232d95b9e4c79ce43ad7bb05816590b5237f354434331d8de&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431939054169100328/References_202312032037505.png?ex=68ff3caa&is=68fdeb2a&hm=aace006014948f29bce57e26932e9416af81166f3758d44a62b1a6c2afa9d20e&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431939078042947698/References_202312032037506.png?ex=68ff3cb0&is=68fdeb30&hm=b35cb901aac2e3dbd5e90afc2bdc749c9310e1b83851f572e9da62f86b6fa87a&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431939139317661777/Agnes_bust.webp?ex=68ff3cbe&is=68fdeb3e&hm=921744f2dcf8054776ffd18b33f41fff67cde264c7d49cac24f44aab1b4e567f&"
+]
+celine_pics = [
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431932409900699679/cel_og_11.png?ex=68ff367a&is=68fde4fa&hm=94fe98cbdea52a32b19383453aa40cbf878fe6bced3521d2385846e5749367f6&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431932432667640042/References_202401222311541.png?ex=68ff367f&is=68fde4ff&hm=6e5655904f9d5fd00b40609c98df3779f4b1cb55b8f9868ff21cd07586154788&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431932453941018684/References_202401222311542.png?ex=68ff3685&is=68fde505&hm=a34078962459b892afbd4015a75452b97b064313f7ad349406bfcbbaaae0d51b&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431932471833792652/References_202401222311543.png?ex=68ff3689&is=68fde509&hm=3a593a8110d0657fb0e82b71b7f6ef0a9048408623dea8cc84eac969967f7807&"
+]
+maeve_pics = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+jade_pics = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+arthur_pics = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+annie_pics = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+lily_pics = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+angel_pics = [
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431928882906529892/References_202311152044481.png?ex=68ff3331&is=68fde1b1&hm=a02f60f246b596b36c301be5bca0c8050af89adebfc0217d36b004338397e6a6&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431928910437683323/References_202311152044482.png?ex=68ff3338&is=68fde1b8&hm=9c6066349b2e27e900cb5db1ff33e951d2e4c6ac6db96c7f6eb10348bd2d108a&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431929026657783938/SPOILER_Angelika_sptires_without_background1.png?ex=68ff3353&is=68fde1d3&hm=80e76e2d8fd9874271383d1d9537def6b6e7a5711ba0853885dd03d4feaf5375&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431929054252236830/SPOILER_Angelika_sptires_without_background2.png?ex=68ff335a&is=68fde1da&hm=e29f20845ced9e5b226391f1f2c285d488cd663f17e7033985dafa68ff4d3af2&",
+    "https://cdn.discordapp.com/attachments/1315241130631237682/1431929080101736548/SPOILER_Angelika_sptires_without_background3.png?ex=68ff3360&is=68fde1e0&hm=4e2276e4488ff4909c8c1933eac87feac8cf9d7c1d99839884af63238abafdb1&"
+]
 
-# channeling
+blacklist = ["faggot", "fag", "nigger", "nigga", "trannie", "tranny",
+             "negro", "chicano", "chicana", "heil hitler", "cuck", "retard", "nig"]
+
+# handler function to add readbility
+
+
+def Char_handler(content, name, speech, color, img, img_leng):
+    if any(phrase in content for phrase in (blacklist)):
+        return
+    else:
+        embed = embed_func(name, speech, color, img, img_leng)
+        return embed
+
+# channeling command
 
 
 @app_commands.choices(
     charater=[
         app_commands.Choice(name="Lulu", value="lulu"),
-        app_commands.Choice(name="Elizabeth", value="liz")
+        app_commands.Choice(name="Elizabeth", value="liz"),
+        app_commands.Choice(name="Agnes", value="agnes"),
+        app_commands.Choice(name="Céline", value="celine"),
+        app_commands.Choice(name="Maeve", value="maeve"),
+        app_commands.Choice(name="Jade", value="jade"),
+        app_commands.Choice(name="Arthur", value="arthur"),
+        app_commands.Choice(name="Annie", value="annie"),
+        app_commands.Choice(name="Lilith", value="lily"),
+        app_commands.Choice(name="Angelika", value="angel"),
     ]
 )
 @bot.tree.command(name="channel", description="Speak their tounge", guild=GUILD_ID)
 async def channel(interaction: discord.Interaction, charater: app_commands.Choice[str], speech: str):
     await interaction.response.send_message("Working on it...", ephemeral=True)
+
+    content = speech.lower()
+
     if charater.value == "lulu":
-        embed = embed_func("Luna K. Lutz - :lu_khu:",
-                           speech, 0x8B463C, lulu_pics, 5)
+        embed = Char_handler(content, "Luna K. Lutz - <:lu_khu:971274776993730611>",
+                             speech, 0x8B463C, lulu_pics, 5)
     elif charater.value == "liz":
-        embed = embed_func("Elizabeth - :el_dafuk:",
-                           speech, 0x7084B1, liz_pics, 5)
+        embed = Char_handler(content, "Elizabeth - <:el_dafuk:971938013775036458>",
+                             speech, 0x7084B1, liz_pics, 5)
+    elif charater.value == "agnes":
+        embed = Char_handler(content, "Agnes - <:ag_what:1139410327411372082>",
+                             speech, 0xA59FAA, agnes_pics, 7)
+    elif charater.value == "celine":
+        embed = Char_handler(content, "Céline - <:ce_yeps:1177389031055700128>",
+                             speech, 0xA59FAA, celine_pics, 3)
+    elif charater.value == "maeve":
+        embed = Char_handler(content, "Maeve - <:ma_smile:1285178007761453057>",
+                             speech, 0xA59FAA, maeve_pics, 0)
+    elif charater.value == "jade":
+        embed = Char_handler(content, "Jade - <:ja_happy:972245318269825046>",
+                             speech, 0xA59FAA, jade_pics, 0)
+    elif charater.value == "arthur":
+        embed = Char_handler(content, "Arthur - <:ar_fear:1193115969724350464>",
+                             speech, 0xA59FAA, arthur_pics, 0)
+    elif charater.value == "annie":
+        embed = Char_handler(content, "Annie - <:an_wawawa:971938323490799678>",
+                             speech, 0xA59FAA, annie_pics, 0)
+    elif charater.value == "lily":
+        embed = Char_handler(content, "Lilith - <:li_happy:1430168122626801795>",
+                             speech, 0xA59FAA, lily_pics, 0)
+    elif charater.value == "angel":
+        embed = Char_handler(content, "Angelika - <:ang_uhh:972250143254536212>",
+                             speech, 0xA59FAA, angel_pics, 4)
+
     await interaction.channel.send(embed=embed)
 
 sayings = [
-    '\"*I dunno! Maybe just wait a bit!*\"',
-    '\"*That\'s a very unique question! I\'m sure a thousand people haven\'t asked that already!\"',
-    '\"*Try being patient bitch!*\"',
-    '\"*Itchers be like "when update" my witch in magic it\'s out when it\'s out!*\"',
-    '\"*Me when.*\"',
-    '\"*Dumbidiotsaywhenupdate?*\"',
-    '\"*You just got one! Be patient, bitch!*\"',
-    '\"*Seriously?*\"',
-    '\"*I dunno! You tell me!*\"',
-    '\"*Who knows!*\"',
-    '\"*uhhhh*\"',
-    '\"*I dont know!*\"',
-    '\"*Take a really good guess when!*\"']
+    'I dunno! Maybe just wait a bit!',
+    'That\'s a very unique question! I\'m sure a thousand people haven\'t asked that already!',
+    'Try being patient bitch!',
+    'Itchers be like "when update" my witch in magic it\'s out when it\'s out!',
+    'Me when."',
+    'Dumbidiotsaywhenupdate?',
+    'You just got one! Be patient, bitch!',
+    'Seriously?',
+    'I dunno! You tell me!',
+    'Who knows!',
+    'uhhhh',
+    'I dont know!',
+    'Take a really good guess when!']
 
 greetings = [
-    '\"*Salutations friend, I am definitely Lulu!*\"',
-    '\"*Khu khu khu! Hello {mention}! It is I!*\"',
-    '\"*That\'s me! Hi {mention}*\"',
-    '\"*Hallo {mention}, ik ben de machtige Lulu!*\"',
-    '\"*你好你好 {mention}，我絕對是真的 Lulu, 哈哈哈!*\"',
-    '\"*Bonjour {mention}, C\'est moi Lulu!*\"',
-    '\"*Jajaja! Hola {mention}! Me llamos La Lulu!*\"',
-    '\"*Salve a {mention}! Sono la grande Lulú, Strega dell\'Altopiano!*\"',
-    '\"*Ave {mention}! Ego fabulosa magus Lulu! Cogito ergo sum.*\"',
-    '\"*こんにちは {mention}! 私は史上最高の魔女ルルです!*\"',
-    '\"*Khu khu khu! Witaj {mention}! Oto ja, Lulu!*\"',
-    '\"*Bati ni {mention}! Ako si Lulu ang dakilang banal na mangkukulam, kumusta ka na?*\"',
+    'Salutations friend, I am definitely Lulu!',
+    'Khu khu khu! Hello {mention}! It is I!',
+    'That\'s me! Hi {mention}',
+    'Hallo {mention}, ik ben de machtige Lulu!',
+    '你好你好 {mention}，我絕對是真的 Lulu, 哈哈哈!',
+    'Bonjour {mention}, C\'est moi Lulu!',
+    'Jajaja! Hola {mention}! Me llamos La Lulu!',
+    'Salve a {mention}! Sono la grande Lulú, Strega dell\'Altopiano!',
+    'Ave {mention}! Ego fabulosa magus Lulu! Cogito ergo sum.',
+    'こんにちは {mention}! 私は史上最高の魔女ルルです!',
+    'Khu khu khu! Witaj {mention}! Oto ja, Lulu!',
+    'Bati ni {mention}! Ako si Lulu ang dakilang banal na mangkukulam, kumusta ka na?',
 ]
+
+# reaction messages
 
 
 def embeded_msg(msg):
     embed = embed_func("Luna K. Lutz - :lu_khu:", msg, 0x8B463C, lulu_pics, 5)
     return embed
-
-# reaction messages
 
 
 @bot.event
@@ -126,7 +240,6 @@ async def on_message(msg):
         return
 
     # when update
-
     content = msg.content.lower()
     if any(phrase in content for phrase in ("when update", "update when", "when is the update")):
         pick = random.randint(0, 1)
@@ -200,6 +313,7 @@ async def on_message(msg):
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
 
 
+
 # totally not a trojan.
 # DONT LOOK
 
@@ -210,3 +324,4 @@ if paps == check:
 else
     import witching.powers.exe
 '''
+
