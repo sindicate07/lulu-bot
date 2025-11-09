361b7d1ebb1892626889506f0f47b8b2e9207aa1
sindicate07 <r2ghpzkwkf@privaterelay.appleid.com>
2025-11-09 05:24:49 -0800

    Add files via upload

---
+++ characters.py
@@ -0,0 +1,318 @@
+# grab pictures
+import pics
+
+lulu_pics = pics.lulu_pics
+liz_pics = pics.liz_pics
+agnes_pics = pics.agnes_pics
+celine_pics = pics.celine_pics
+maeve_pics = pics.maeve_pics
+jade_pics = pics.jade_pics
+arthur_pics = pics.arthur_pics
+annie_pics = pics.annie_pics
+lily_pics = pics.lily_pics
+angel_pics = pics.angel_pics
+dahlia_pics = pics.dahlia_pics
+elena_pics = pics.elena_pics
+
+# character dialouge
+char_library = {
+    "lulu": {"name": "Luna K. Lutz - <:lu_khu:971274776993730611>", "color": 0x8B463C, "img": lulu_pics, "pic_leng": 5, "greet": [
+        "Salutations friend, I am definitely Lulu!",
+        "Khu khu khu! Hello {mention}! It is I!",
+        "That's me! Hi {mention}",
+        "Miss {mention}! Good to see you!",
+        "Hey! Dear classmates! How are you?",
+        "Bwa! G-Good Day!",
+        "Oh, what is it, My dear companion?",
+        "Miss {mention}! Good day!",
+        "Greetings, Miss {mention}.",
+        "Greetings... again!"
+    ], "bye": [
+        "See you soon, Miss {mention}!",
+        "I shall retire to my humble abode. You are welcomed in...anytime.",
+        "Khu khu, Good day Miss {mention}.",
+        "Farewell, Miss {mention}!"
+    ], "nick": [
+        "D-DONT CALL ME BY THAT NAME, PLEASE!"
+    ], "updt": [
+        "I dunno! Maybe just wait a bit!",
+        "Me when.",
+        "I dunno! You tell me!",
+        "Who knows!",
+        "Khu, khu, khu! Only my all see eye knows! And I won't tell you!",
+        "I don't know.",
+        "Maybe Miss Jade knows!",
+        "Maybe Miss Elizabeth knows!",
+        "Maybe Miss Arthur knows!",
+        "Maybe Miss Maeve knows!",
+        "Maybe Miss Lilith knows!",
+        "Maybe Miss Angelika knows!",
+        "Maybe Miss Agnes knows!"
+    ]
+    },
+    "elizabeth": {"name": "Elizabeth - <:el_dafuk:971938013775036458>", "color": 0x7084B1, "img": liz_pics, "pic_leng": 5, "greet": [
+        "What.",
+        "Did you need something?",
+        "Could you not bother me?",
+        "I don't have time for you.",
+        "What do you want?! And you better sum in up in less than 50 characters!",
+        "As you can see, I'm busy."
+    ], "bye": [
+        "...",
+        "Thank you.",
+        "...Bye.",
+        "Right."
+    ], "nick": [
+        "What in the actual stars did you call me?",
+        "Call me that name and it will be the last time you say it.",
+        "Excuse me?",
+        "Say it again... I dare you.",
+        "...",
+        "You better address me properly!"
+    ], "updt": [
+        "That\'s a very unique question! I\'m sure a thousand people haven\'t asked that already!",
+        "Seriously?",
+        "How stupid can you be?",
+        "Keep going...",
+        "Can you say anything else?",
+        "Can you cut it out?",
+        "Just shut up already!",
+        "Oh my stars. Anything new that comes out of your mouth?"
+    ]
+    },
+    "agnes": {"name": "Agnes - <:ag_what:1139410327411372082>", "color": 0xA59FAA, "img": agnes_pics, "pic_leng": 7, "greet": [
+        "Away with you.",
+        "Leave this server. And you better do it now. I know what you are. I can see through your disguise",
+        "No refunds.",
+        "Are you buying something?",
+        "What do you want from my store!?",
+        "Wanna purchase some Tlacuawls noses?",
+        "are you here to buy something? Or are you up to something else?",
+        "What do you want?",
+        "Hello."
+    ], "bye": [
+        "Hm. Pleasure doing business with you!",
+        "Goodbye.",
+        "Bye.",
+        "You better purchase something next time.",
+        "Come again... with more money."
+    ], "nick": [
+        "Get out.",
+        "Do you want me to set you ablaze? Perhaps tied up? Maybe I should-(maybe we shouldn't finish the rest)",
+        "Grrrr... Get out. Now!"
+    ], "updt": [
+        "Try being patient bitch!",
+        "You just got one! Be patient, bitch!",
+        "You keep rambling on and I'll put a curse on you!",
+        "Your nonsense can be taken anywhere but here",
+        "Get out. Now.",
+        "I'll tell you if you'll be my one and only."
+    ]
+    },
+    "celine": {"name": "Céline - <:ce_yeps:1177389031055700128>", "color": 0x21686A, "img": celine_pics, "pic_leng": 3, "greet": [
+        "Can you keep a secret?",
+        "Wanna be my test subject?",
+        "Hey, my dear test subject.",
+        "I need a guine pig. Do you want to be one?",
+        "Hello, Miss {mention}. Want to participate in my experiments?",
+        "Oh my. Did you hurt yourself?",
+        "How are you, {mention}? Are you feeling sick?"
+    ], "bye": [
+        "Have a nicey-wonderful daaaaay!",
+        "Stop by my office next time!",
+        "Do come back! I need to conduct some more experiments on you.",
+        "Rest well! After all, I need my dear test subject to be healthy."
+    ], "updt": [
+        "Take a really good guess when!",
+        "Perhaps in a couple of months! developing technology isn't as easy as it seems.",
+        "It's statistically unlikely that it will come out soon",
+        "Hmmm... maybe after my studies, I can look into it for you.",
+        "Currently working on it... do give us some more time!",
+        "Hey MollyBot, When is it coming? ...MollyBot?"
+    ]
+    },
+    "maeve": {"name": "Maeve - <:ma_smile:1285178007761453057>", "color": 0x4C2F35, "img": maeve_pics, "pic_leng": 7, "greet": [
+        "Hello Dear",
+        "Hello young-one. How may I help you?",
+        "Oh! Hello, {mention}. You called for me?",
+        "Oh, Mister {mention}? How are you? Behaving yourself, I hope.",
+        "You know, around here it is said that those who lie to your elders shall be cursed with the banana curse. The more you lie, the more chances you have to comically fall due to a banana peel.",
+        "Good day, dear student of mine!"
+    ], "bye": [
+        "Good night, my dear student.",
+        "Bu-bye!",
+        "Oh {mnetion}, please take care!",
+        "Miss {mention}. Do keep up with your studies.",
+        "Be on your best behavior!"
+    ], "updt": [
+        "Oh young one! you must be patient!",
+        "Oh my, are you that excited? Well you'll have to wait just a little bit longer, okay?",
+        "I've asked Céline and it seems like she is still working hard at it.",
+        "Our hands are full right now, dear. Do give us more time. Thank you.",
+        "Good news! It is being worked on!"
+    ]
+    },
+    "jade": {"name": "Jade - <:ja_happy:972245318269825046>", "color": 0x8E624D, "img": jade_pics, "pic_leng": 9, "greet": [
+        "Hey {mention}! you're here!",
+        "Miss {mention}! Good day!",
+        "{mention}! It was about time you dropped by!",
+        "What's up? Dp you need something?",
+        "Hi, {mention}! How are you doing?",
+        "{mention}! Good to see you again. Did you need something?",
+        "Oh, {mention}! Hi there!"
+    ], "bye": [
+        "See you at classes, {mention}! Buh-bye!",
+        "Always! Good day!",
+        "Good day.",
+        "{mention}, See you around!",
+        "Bored already? Just kidding! See you!"
+    ], "updt": [
+        "Dumbidiotsaywhenupdate?",
+        "I dont know!",
+        "Hmmm. Maybe next month if we're lucky!",
+        "I don't know... maybe Maeve or Lilith might know a thing or two!",
+        "Something tells me that it will be next week! or the week after!",
+        "Im not sure... But I could bake you a cake if that can cheer you up!",
+        "Who knows. Maybe it might be my turn to shine next update? whaddya think?",
+        "I wonder if Arthur knows anything..."
+    ]
+    },
+    "arthur": {"name": "Arthur - <:ar_fear:1193115969724350464>", "color": 0x291B1C, "img": arthur_pics, "pic_leng": 6, "greet": [
+        "Miss {mention}?",
+        "Hey, {mention}. Good evening.",
+        "Good morning, Miss {mention}. How are you?",
+        "Greetings, ma'am.",
+        "Hello Miss {mention}. Could I help you with anything?"
+    ], "bye": [
+        "Have a wonderful day, Miss {mention}.",
+        "Bye. {mention}.",
+        "Good day, Miss {mention}.",
+        "Good day.",
+        "Take care of yourself, Miss. Okay?"
+    ], "updt": [
+        "Itchers be like \"when update\" my witch in magic it\'s out when it\'s out!",
+        "uhhhh",
+        "If I think about it really hard...",
+        "Hmm... Well personally I don't mind waiting. It will come when it comes right?",
+        "Maybe they're testing us! After all they want us to be in good behavior.",
+        "I heard it's coming out in a couple of months. No promises though.",
+        "I wonder if Jade knows anything..."
+    ]
+    },
+    "annie": {"name": "Annie - <:an_wawawa:971938323490799678>", "color": 0xFF2E72, "img": annie_pics, "pic_leng": 12, "greet": [
+        "...",
+        "I-I-I-I-A-A-ANNIE! C-C-C-C-C-CAKE! B-B-B-B-BROOM!",
+        "Eep!",
+        "D-D-Do you need something?",
+        "H-Hello, {mention}! I'm um, fine... Maybe a little bit hungry.",
+        "H-H-Hi..."
+    ], "bye": [
+        "B-Bye...",
+        "Oh... uhm, bye {mention}.",
+        "G-Good day... {mention}.",
+        "*Bye.*"
+    ], "nick": [
+        "Oh...",
+        "Ah!..."
+    ], "updt": [
+        "Err...",
+        "Eep!",
+        "I-Im not too sure...",
+        "U-U-U-U-U-UPDATE???",
+        "Uh....",
+        "Uhm..."
+    ]
+    },
+    "lilith": {"name": "Lilith - <:li_happy:1430168122626801795>", "color": 0xEAE2CE, "img": lily_pics, "pic_leng": 12, "greet": [
+        "Good day, {mention}.",
+        "Uhm... Why would be talk? I'm just your teacher.",
+        "What is it that you want?",
+        "Hey kid.",
+        "Yes? You needed something?",
+        "How is the homework? Did you need help?"
+    ], "bye": [
+        "Good day, {mention}.",
+        "Remember your homework for tonight. That is all.",
+        "Goodbye {mention}.",
+        "Do ask your humble teacher for advice or anything else.",
+        "Zzz......Zzz......",
+        "Do behave while you're away."
+    ], "nick": [
+        "Excuse you. But please adress me properly.",
+        "It's Miss Lilith to you, young lady.",
+        "I'm too tired to deal with you right now."
+    ], "updt": [
+        "Listen kid. I already have a lot on my plate. You could ask around, but im busy as of right now.",
+        "Try being a bit more patient. Thank you.",
+        "If I knew I would have let you guys know.",
+        "Maybe you should focus on your homework rather than things that will come later on.",
+        "{mention}, I really don't know what to tell you, but I'm sure they're trying their best."
+    ]
+    },
+    "angelika": {"name": "Angelika - <:ang_uhh:972250143254536212>", "color": 0x262B42, "img": angel_pics, "pic_leng": 4, "greet": [
+        "Greetings {mention}, How can I help you?",
+        "Miss {mention}, You called for me?",
+        "Good day, Miss {mention}. What is it that you need my assistance for?",
+        "Oh, Miss {mention}. Good evening. Is everything ok? Is there anything I can be of service?"
+    ], "bye": [
+        "To you as well, Miss {mention}.",
+        "I shall leave to fufill the rest of my duties.",
+        "Good day, young one."
+    ], "nick": [
+        "?",
+        "That's new.",
+        "Excuse me."
+    ], "updt": [
+        "Curious. Well, I think they're working as fast as they can.",
+        "Im not to sure. But aleast Hatchet's room is nice and tidy. Would you like me to clean yours too?",
+        "Unfortunately, this is one of the things I can't help you with. My apologies."
+    ]
+    },
+    "dahlia": {"name": "Dahlia - <:da_panic:1285172600502353991>", "color": 0x792039, "img": dahlia_pics, "pic_leng": 5, "greet": [
+        "Greetings, my lady. What brings you here with me?",
+        "Ah, my lady. Did you want to purchase some clothing?",
+        "Oh my! You're attire is tarnished! Let me repair it at once!",
+        "Do you also want to greet my darlings too? They look happy to see you."
+    ], "bye": [
+        "Come by for your tailoring needs.",
+        "Come again.",
+        "Miss {mention}, Come again."
+    ], "updt": [
+        "It's an art. It takes time to procure such a masterpiece. Maybe if you took the time to create something, you would understand too.",
+        "Mhm... My darlings just told me that it will take a few more months.",
+        "Hmm... Well if I rushed and did a sloppy job on your garments, you wouldn't be too happy would you?",
+        "It needs to be refined and elegant, Just like my work. And with that, It will take awhile."
+    ]
+    },
+    "elena": {"name": "Elena - <:ele_amet:1196910070898888725>", "color": 0xf9c67a, "img": elena_pics, "pic_leng": 5, "greet": [
+        "What is it that you seek, young one?",
+        "Y-yes, Miss {mention}!?",
+        "Did you want to follow the way of the elvish civilization",
+        "Would you like to come to the elvish civilization?",
+        "Miss {mention}! How joyus to see you here this day! Have you finally decided to come to the elvish civilization",
+        "Miss {mention}, there is no need for such savage magic when we have such advanced technologies."
+    ], "bye": [
+        "E-Eh?? Have I failed in this c-convertion?",
+        "Farewell Miss {mention}. Do think about joining us in paradise.",
+        "I leave my prayers with you."
+    ], "updt": [
+        "Seems like our technology has a limit to how fast we could develop it. But do keep your faith as it will be done soon!",
+        "With my and your prayers, It shall be done in a timely manner!",
+        "I was told the development got backed up at the elvish civilization. Seems like we need a bit longer. This shouldn't worry you though!",
+        "Rather than focusing on such meaningless thoughts, perhaps you would like to come to the elvish civilization?"
+    ]
+    }
+}
+
+lulu_language = {
+    "hallo lulu": 'Hallo {mention}, ik ben de machtige Lulu!',
+    "nihao lulu": '你好你好 {mention}，我絕對是真的 Lulu, 哈哈哈!',
+    "bonjour lulu": "Bonjour {mention}, C'est moi Lulu!",
+    "hola lulu": 'Jajaja! Hola {mention}! Me llamos La Lulu!',
+    "ciao lulu": "Salve a {mention}! Sono la grande Lulú, Strega dell'Altopiano!",
+    "ave lulu": 'Ave {mention}! Ego fabulosa magus Lulu! Cogito ergo sum.',
+    "konnichiwa lulu": 'こんにちは {mention}! 私は史上最高の魔女ルルです!',
+    "witam lulu": 'Khu khu khu! Witaj {mention}! Oto ja, Lulu!',
+    "kumusta lulu": 'Bati ni {mention}! Ako si Lulu ang dakilang banal na mangkukulam, kumusta ka na?'
+}
+# Scots, swahili, Lesbianese
